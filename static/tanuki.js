d53f9ae0937a8efa9cec646e5044d28c8a47d46f
siznax <steve@siznax.net>
2013-05-20 21:57:42 -0700

    support multiple YouTube playlists in a single entry.

--- static/tanuki.js
+++ static/tanuki.js
@@ -9,7 +9,7 @@ $( function() {
 	var lat  = $( "div.leafletjs" ).attr("lat");
 	var lon  = $( "div.leafletjs" ).attr("lon");
 	var zoom = $( "div.leafletjs" ).attr("zoom");
-	// alert("found map: " + map + " [" + lat + "," + lon + "] " + zoom );
+	console.log("found map: " + map + " [" + lat + "," + lon + "] " + zoom );
 	map = L.map( map ).setView( [lat,lon],zoom );
 	var API_KEY = $( "#leafletjs_api_key" ).attr("key");
 	L.tileLayer('http://{s}.tile.cloudmade.com/'+API_KEY+'/997/256/{z}/{x}/{y}.png', { attribution:'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>', maxZoom:18 }).addTo( map );
@@ -17,23 +17,27 @@ $( function() {
 
     // Youtube playlist ----------------------------------------
 
-    if ( $("#playlist").length ) {
-	// alert("found playlist");
-	if ( $("#viewtube").length==0 ) {
-	    $( "#playlist" ).prepend("<div id=\"viewtube\"></div>");
+    $("div.playlist").each( function() {
+	playlist = $(this).attr("id");
+	console.log( "found playlist: " + playlist );
+	// setup one viewtube only
+	if ( $( "#viewtube" ).length==0 ) {
+	    $( "#"+playlist ).prepend("<div id=\"viewtube\"></div>");
+	    first_video = $( "#"+playlist+" a" ).attr("id");
+	    changeChannel( first_video );
 	}
-	changeChannel( $("#playlist a")[0].id );
-    }
+    });
 
-    $( "#playlist a" ).click( function( event ) {
+    $( ".playlist a" ).click( function( event ) {
 	changeChannel( event.target.id );
 	return false;
     });
 
-    function changeChannel( id ) {
+    function changeChannel( video ) {
+	console.log( "changeChannel(" + video + ")");
 	$( "#viewtube" ).html('<iframe width="420" height="315"'
 			      + ' src="http://www.youtube.com/embed/'
-			      + id + '" frameborder="0" allowfullscreen>'
+			      + video + '" frameborder="0" allowfullscreen>'
 			      + '</iframe>');
     }
 
