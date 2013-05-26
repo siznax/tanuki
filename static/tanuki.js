feae87e1b1144cdb4d00513f56aefec90bf9907e
siznax <steve@siznax.net>
2013-05-26 15:13:52 -0700

    added image gallery JS.

--- static/tanuki.js
+++ static/tanuki.js
@@ -9,7 +9,7 @@ $( function() {
 	var lat  = $( "div.leafletjs" ).attr("lat");
 	var lon  = $( "div.leafletjs" ).attr("lon");
 	var zoom = $( "div.leafletjs" ).attr("zoom");
-	console.log("found map: " + map + " [" + lat + "," + lon + "] " + zoom );
+	console.log("TANUKI found map: " + map + " [" + lat + "," + lon + "] " + zoom );
 	map = L.map( map ).setView( [lat,lon],zoom );
 	var API_KEY = $( "#leafletjs_api_key" ).attr("key");
 	L.tileLayer('http://{s}.tile.cloudmade.com/'+API_KEY+'/997/256/{z}/{x}/{y}.png', { attribution:'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>', maxZoom:18 }).addTo( map );
@@ -19,7 +19,7 @@ $( function() {
 
     $("div.playlist").each( function() {
 	playlist = $(this).attr("id");
-	console.log( "found playlist: " + playlist );
+	console.log( "TANUKI found playlist: " + playlist );
 	// setup one viewtube only
 	if ( $( "#viewtube" ).length==0 ) {
 	    $( "#"+playlist ).prepend("<div id=\"viewtube\"></div>");
@@ -54,5 +54,61 @@ $( function() {
 	    .css( "list-style-type", "circle" );
     }
 
+    // IMAGE GALLERY -------------------------------------------
+
+    if ( $(".gallery").length ) {
+	var id = $( ".gallery" ).closest("div[id]").attr("id");
+	var num = $(".gallery img").length;
+	console.log( "TANUKI found gallery id=" + id + " (" + num + ")" );
+    }
+
+    $( ".gallery" ).click( function() {
+	if ( $("#slide").length == 0 ) {
+	    console.log( "+ append #slide");
+	    $("body").append("<div id=slide />");
+	    $("#slide").attr( "num", $(".gallery img").length );
+	    putSlide( event.target.src );
+	}
+    });
+
+    $( "body" ).on( "click", "#slide img", function() {
+	nextSlide( event.target.src );
+    });
+
+    $("body").on( "click", "#slide", function() {
+	if ( event.target.id=="slide" ) {
+	    removeSlide();
+	}
+    });
+
+    function putSlide( src ) {
+	console.log("+ put slide " + src);
+	$("#slide").html( '<img src='+src+'>' );
+	$("#slide").css( "top",$("body").scrollTop() );
+	$("#slide").css("display","block");
+	$("body").css("overflow","hidden");
+    }
+
+    function nextSlide( src ) {
+	var last_index = $("#slide").attr("num") - 1;
+	$(".gallery img").each( function(i) {
+	    if (this.src==event.target.src) {
+		console.log("+ clicked slide " + i);
+		var next_index = i;
+		if ( next_index >= last_index ) {
+		    removeSlide();
+		} else {
+		    console.log( "+ next slide " + (i+1) );
+		    putSlide( $(".gallery img")[i+1].src );
+		}
+	    }
+	});
+    }
+
+    function removeSlide() {
+	$( "#slide" ).remove();
+	$( "body" ).css( "overflow","auto" );
+	console.log( "+ #slide removed" );
+    }
 
 });
