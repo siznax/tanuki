9ea18edf6c5930d3743da9ec247461f990773470
siznax <steve@siznax.net>
2013-06-05 14:16:40 -0700

    rewrite gallery.js. support ESC,j,k,s keys. understanding timing better.

--- static/gallery.js
+++ static/gallery.js
@@ -2,128 +2,164 @@
 
 $( function() {
 
-    var DEBUG = false;
+    var DEBUG = true;
+    var g = new Object();
 
+    // find galleries
     if ( $(".galleryjs img").length ) {
 	$( ".galleryjs" ).each( function() {
 	    var gid = $( this ).closest("div[id]").attr("id");
 	    var num = $( "#" + gid + " img").length;
+	    g[gid] = new Object();
+	    g[gid]['num'] = num;
+	    if ( ! g.hasOwnProperty('id') ) { // set id to first found
+		g['id'] = gid;
+	    }
 	    console.log( "TANUKI found gallery #" + gid + " num=" + num );
-	    initSlide( gid,num );
 	});
+	console.log( g );
     }
 
-    function initSlide( gid, num ) {
-	if ( $("#slide").length == 0 ) {
-    	    if ( DEBUG ) { 
-    		console.log( "+ append #slide gid=" + gid + " num=" + num );
-    	    }
-    	    $("body").append("<div id=slide><img></div>");
-    	    $("#slide img").bind( "load", sizeSlide );
-	}
-    }
-
+    // click gallery img
     $( ".galleryjs img" ).click( function() {
-	var gid = $( event.target ).closest("div[id]").attr("id");
-	var num = $("#" + gid + " img").length;
-	initSlide( gid, num );
-    	$("#slide").attr( "gid",gid );
-    	$("#slide").attr( "num",num );
-	putSlide( event.target.src );
+	var gid = $( this ).closest("div[id]").attr("id");
+	g['id'] = gid;
+	var src = event.target.src;
+	var i = getIndex( src );
+	console.log("+ clicked gallery #"+gid+" img["+i+"] "+src);
+	putSlide( src );
     });
 
-    function sizeSlide() {
-	var width = $("#slide img").width();
-	var height = $("#slide img").height();
-	var aspect = null;
-	if ( width > height ) {
-	    aspect = "WIDE";
-	} else {
-	    aspect = "TALL";
-	}
-	slideReport();
-	if ( aspect=='WIDE' ) {
-	    if ( width > $("#slide").width() ) {
-	    	wideView( aspect );
-	    } else {
-		unscaledView( aspect );
-	    }
-	} else if ( aspect=='TALL' ) {
-	    if ( height > $("#slide").height() ) {
-	    	tallView( aspect );
-	    } else {
-		unscaledView( aspect );
-	    }
-	} else {
-	    unscaledView( aspect );
-	}
-	if ( DEBUG ) {
-	    var width = $("#slide img").width();
-	    var height = $("#slide img").height();
-	    console.log( "+ img loaded " + width + "x" + height );
-        }
-    }
-
-    function slideReport() {
-       var width = $("#slide img").width();
-       var height = $("#slide img").height();
-       var slide_height = $("#slide").height();
-       var slide_width = $("#slide").width();
-       if ( DEBUG ) {
-	   sdim = "slide " + slide_height + "x" + slide_width;
-	   idim = "img " + width + "x" + height;
-	   console.log( "> " + sdim + " " + idim );
-       }
-    }
-
-    function wideView( aspect ) {
-       if ( DEBUG ) {
-	   console.log("> aspect " + aspect + " view WIDE");
-       }
-       var slide_height = $("#slide").height();
-       $( "#slide img" ).css("width","100%");
-       $( "#slide img" ).css("height","auto");
-       var scaled_height = $("#slide img").height()
-       var margin_top = Math.floor( (slide_height-scaled_height)/2 );
-       $( "#slide img" ).css( "margin-top",margin_top );
-   }
-
-   function tallView( aspect ) {
-       if ( DEBUG ) {
-	   console.log("> aspect " + aspect + " view TALL");
-       }
-       $( "#slide img" ).css("width","auto");
-       $( "#slide img" ).css("height","100%");
-       $( "#slide img" ).css( "margin-top",0 );
-    }
-
-    function unscaledView( aspect ) {
-	if ( DEBUG ) {
-	    console.log("> aspect " + aspect + " view NOSCALE");
-	}
-	var img_height = $("#slide img").height();
-	var slide_height = $("#slide").height();
-	var margin_top = Math.floor( (slide_height-img_height)/2 );
-	$( "#slide img" ).css( "margin-top",margin_top );
-    }
-
+    // click slide img
     $( "body" ).on( "click", "#slide img", function() {
-	var gid = $("#slide").attr("gid");
-	var src = event.target.src
-	nextSlide( gid,src );
+	var src = event.target.src;
+	var gid = g['id'];
+	var i = getIndex( src );
+	if ( DEBUG ) {
+	    console.log("+ clicked slide img #" + gid + "[" + i+ "] " + src );
+	}
+	nextSlide( gid,i );
     });
 
+    // click slide (but not img)
     $("body").on( "click", "#slide", function() {
 	if ( event.target.id=="slide" ) {
-	    $("#slide img").off();
 	    removeSlide();
 	}
     });
 
-    function putSlide( src, index ) {
-	if ( DEBUG ) {
-	    console.log("+ put slide[" + index + "] " + src );
+    $( document ).keyup( function(e) {
+	// console.log( "pressed key "+ e.keyCode );
+	if (e.keyCode==27) { // pressed ESC
+	    removeSlide(); 
 	}
+	if (e.keyCode==74) { // pressed j (NEXT)
+	    if ( $("#slide").length ) {
+		nextSlide( g['id'],getIndex($("#slide img")[0].src) ); 
+	    } else {
+		nextSlide( g['id'],-1 );
+	    }
+	}
+	if (e.keyCode==75) { // pressed k (PREV)
+	    if ( $("#slide").length ) {
+		prevSlide( g['id'],getIndex($("#slide img")[0].src) ); 
+	    } else {
+		last = g[g['id']]['num'];
+		prevSlide( g['id'],last );
+	    }
+	}
+	if (e.keyCode==83) { // pressed s (SIZE)
+	    if ( $("#slide img").length ) {
+		sizeImg();
+	    }
+	}
+    });
+
+    function initSlide() {
+	if ( $("#slide").length == 0 ) {
+    	    $("body").append("<div id=slide><img></div>");
+    	    $("#slide img").bind( "load", slowSizeImg ); // may fire X times
+    	    if ( DEBUG ) { 
+    		console.log( "+ append/bind #slide img");
+    	    }
+	}
+    }
+
+    function setAspect( iw,ih ) {
+	g['aspect'] = "WIDE";
+	if ( ih > iw ) {
+	    g['aspect'] = "TALL";
+	}
+    }
+
+    function slowSizeImg() {
+	window.setTimeout( sizeImg, 100 );
+	window.setTimeout( setMarginTop, 150 );
+    }
+
+    function sizeImg() {
+	if ( $("#slide").length==0 ) {
+	    return false;
+	}
+	var sw = $("#slide").width();
+	var sh = $("#slide").height();
+	var iw = $("#slide img")[0].naturalWidth;
+	var ih = $("#slide img")[0].naturalHeight;
+	console.log( "+ sizeImg slide " + sw+"x"+sh + " img " + iw+"x"+ih );
+	setAspect( iw,ih );
+	if ( (iw>sw) && (ih>sh) ) { tallView(); }
+	else if ( iw > sw ) { wideView(sh); }
+	else if ( ih > sh ) { tallView(sh); }
+	else { unscaled(); }
+	if ( DEBUG ) {
+	    var scaled = $("#slide img").width() 
+		+ "x" + $("#slide img").height();
+	    var natural = $("#slide img")[0].naturalWidth 
+		+ "x" + $("#slide img")[0].naturalHeight;
+	    console.log( "  img scaled " + scaled + " natural " + natural );
+        }
+    }
+
+    function wideView( sh ) {
+	if ( DEBUG ) {
+	    console.log("  aspect " + g['aspect'] + " view WIDE");
+	}
+	$( "#slide img" ).css("width","100%");
+	$( "#slide img" ).css("height","auto");
+    }
+
+    function tallView() {
+	if ( DEBUG ) {
+	    console.log("  aspect " + g['aspect'] + " view TALL");
+	}
+	$( "#slide img" ).css("width","auto");
+	$( "#slide img" ).css("height","100%");
+	$( "#slide img" ).css( "margin-top",0 );
+    }
+
+    function unscaled() {
+	if ( DEBUG ) {
+	    console.log("  aspect " + g['aspect'] + " view UNSCALED");
+	}
+	var sh = $("#slide").height();
+	var ih = $("#slide img").height();
+    }
+
+    function setMarginTop() {
+	var slide_height = $("#slide").height();
+	var image_height = $("#slide img").height();
+	var top = Math.floor( (slide_height - image_height)/2 );
+	if (DEBUG) {
+	    console.log( "  setMarginTop sh=" + slide_height + " ih=" + image_height );
+	}
+	$( "#slide img" ).css( "margin-top",top );
+    }
+
+    function putSlide( src ) {
+	if ( DEBUG ) {
+	    console.log( "+ putSlide #" + g['id'] + "[" + getIndex( src ) + "] " + src );
+	}
+	initSlide();
 	$("#slide img").attr("src",src);
 	$("#slide img").css("width","auto");
 	$("#slide img").css("height","auto");
@@ -132,34 +168,49 @@ $( function() {
 	$("body").css("overflow","hidden");
     }
 
-    function nextSlide( gid, src ) {
-	var next = 0;
-	var last = $("#slide").attr("num") - 1;
-	$("#" + gid + " img").each( function(i) {
-	    if (this.src==src) {
-		clicked = i;
-		next = i + 1;
+    function getIndex( src ) {
+	if ( ( ! src ) || ( ! g['id'] ) ) { 
+	    alert("tanuki gallery.js IndexError");
+	}
+	var found = 0;
+	$("#" + g['id'] + " img").each( function(i) {
+	    if (this.src==src) {  
+		found=i; 
 	    }
 	});
+	return found;
+    }
+
+    function nextSlide( gid,i ) {
+	var next = i+1;
 	if ( DEBUG ) {
-	    console.log( "> #" + gid + " clicked " + clicked + " next " + next );
+	    console.log( "+ nextSlide #" + gid + " " + i + "=>" + next );
 	}
-	if ( next > last ) {
+	if ( next > g[gid]['num']-1 ) {
 	    removeSlide();
 	    return false;
-	} 
-	var next_image = $("#" + gid + " img")[next].src;
-	if ( DEBUG ) {
-	    console.log( "+ #" + gid + " next image[" + next + "] " + next_image );
 	}
-	putSlide( next_image, next );
+	putSlide( $("#"+gid+" img")[next].src );
+    }
+
+    function prevSlide( gid,i ) {
+	var prev = i-1;
+	if ( DEBUG ) {
+	    console.log( "+ prevSlide #" + gid + " " + i + "=>" + prev );
+	}
+	if ( prev < 0 ) {
+	    removeSlide();
+	    return false;
+	}
+	putSlide( $("#"+gid+" img")[prev].src );
     }
 
     function removeSlide() {
+	$("#slide img").off();
 	$( "#slide" ).remove();
 	$( "body" ).css( "overflow","auto" );
 	if ( DEBUG ) {
-	    console.log( "+ #slide removed" );
+	    console.log( "+ removed #slide" );
 	}
     }
 
